1c4fd16fe8ddcf8ae2c105c4412fee0389d034c4
AmoghK22 <Amoghk2005@gmail.com>
2026-04-23 17:09:26 +0530

    removed extra input fields from price prediction

--- client/src/constants/data.js
+++ client/src/constants/data.js
@@ -6,7 +6,7 @@ export const STATES = [
 ];
 
 export const DISTRICT = {
-  'Maharashtra': ['Ahmednagar', 'Akola', 'Amarawati', 'Aurangabad', 'Beed', 'Bhandara', 'Buldhana', 'Chandrapur', 'Dhule', 'Gadchiroli', 'Gondia', 'Hingoli', 'Jalgaon', 'Jalna', 'Kolhapur', 'Latur', 'Mumbai', 'Nagpur', 'Nanded', 'Nandurbar', 'Nashik', 'Osmanabad', 'Parbhani', 'Pune', 'Raigad', 'Ratnagiri', 'Sangli', 'Satara', 'Solapur', 'Thane', 'Wardha', 'Yeotmal', 'Yavatmal'],
+  'Maharashtra': ['Ahmednagar', 'Akola', 'Amarawati', 'Aurangabad', 'Beed', 'Bhandara', 'Buldhana', 'Chandrapur', 'Dhule', 'Gadchiroli', 'Gondia', 'Hingoli', 'Jalgaon', 'Jalna', 'Kolhapur', 'Latur', 'Nagpur', 'Nanded', 'Nandurbar', 'Nashik', 'Osmanabad', 'Parbhani', 'Pune', 'Raigad', 'Ratnagiri', 'Sangli', 'Satara', 'Solapur', 'Thane', 'Wardha', 'Yeotmal', 'Yavatmal'],
   'Punjab': ['Amritsar', 'Bhatinda', 'Ferozpur', 'Gurdaspur', 'Hoshiarpur', 'Jalandhar', 'Kapurthala', 'Ludhiana', 'Moga', 'Patiala', 'Roopnagar / Ropar', 'Sangrur'],
   'Karnataka': ['Bagalkot', 'Bangalore', 'Belgaum', 'Bellary', 'Bidar', 'Chitradurga', 'Dakshina Kannada', 'Dharwad', 'Gulbarga / Kalaburagi', 'Hassan', 'Kolar', 'Koppal', 'Mandya', 'Mysore', 'Raichur', 'Shimoge', 'Tumkur', 'Uttara Kannada'],
   'Gujarat': ['Ahmedabad', 'Amreli', 'Anand', 'Banaskantha', 'Bharuch', 'Bhavnagar', 'Dangs', 'Kutch', 'Mehsana', 'Panchmahal', 'Rajkot', 'Sabarkantha', 'Surat', 'Surendranagar', 'Vadodara / Baroda', 'Valsad'],
@@ -14,16 +14,16 @@ export const DISTRICT = {
   'Madhya Pradesh': ['Balaghat', 'Betul', 'Bhopal', 'Burhanpur', 'Chhatarpur', 'Chhindwara', 'Damoh', 'Datia', 'Dewas', 'Dhar', 'Guna', 'Gwalior', 'Hoshangabad', 'Indore', 'Jabalpur', 'Jhabua', 'Katni', 'Khargone / West Nimar', 'Mandla', 'Mandsaur', 'Morena', 'Narsinghpur', 'Panna', 'Raisen', 'Rajgarh', 'Ratlam', 'Rewa', 'Sagar', 'Satna', 'Sehore', 'Seoni / Shivani', 'Shahdol', 'Shajapur', 'Shivpuri', 'Sidhi', 'Tikamgarh', 'Ujjain', 'Vidisha'],
   'West Bengal': ['24 Parganas', 'Bankura', 'Bardhaman', 'Birbhum', 'Burdwan', 'Cooch Behar', 'Darjeeling', 'Hooghly', 'Howrah', 'Jalpaiguri', 'Kolkata', 'Malda', 'Murshidabad', 'Nadia', 'Purnea', 'Midnapur'],
   'Tamil Nadu': ['Chengalpattu MGR / Kanchipuram', 'Coimbatore', 'Madurai', 'North Arcot / Vellore', 'Salem', 'South Arcot / Cuddalore', 'Thanjavur', 'Thirunelveli', 'Tiruchirapalli / Trichy'],
-  'Bihar': ['Bhagalpur', 'Darbhanga', 'Gaya', 'Gopalganj', 'Mungair', 'Muzaffarpur', 'Patna', 'Saran', 'Sitapur'],
+  'Bihar': ['Bhagalpur', 'Darbhanga', 'Gaya', 'Muzaffarpur', 'Patna', 'Saran', 'Sitapur'],
   'Rajasthan': ['Ajmer', 'Alwar', 'Banswara', 'Barmer', 'Bharatpur', 'Bhilwara', 'Bikaner', 'Bundi', 'Chittorgarh', 'Churu', 'Dungarpur', 'Ganganagar', 'Jaipur', 'Jaisalmer', 'Jalore', 'Jhalawar', 'Jhunjhunu', 'Jodhpur', 'Kota', 'Nagaur', 'Pali', 'Rajsamand', 'Sikar', 'Sirohi', 'Tonk', 'Udaipur'],
   'Haryana': ['Ambala', 'Bhiwani', 'Gurgaon', 'Hissar', 'Jind', 'Karnal', 'Kurukshetra', 'Mahendragarh / Narnaul', 'Rohtak'],
   'Andhra Pradesh': ['Ananthapur', 'Chittoor', 'East Godavari', 'Guntur', 'Kadapa YSR', 'Krishna', 'Kurnool', 'S.P.S. Nellore', 'Visakhapatnam', 'West Godavari'],
   'Telangana': ['Adilabad', 'Karimnagar', 'Khammam', 'Medak', 'Nizamabad', 'Warangal'],
   'Kerala': ['Alappuzha', 'Eranakulam', 'Kannur', 'Kollam', 'Kottayam', 'Kozhikode', 'Malappuram', 'Palakkad', 'Thiruvananthapuram', 'Thrissur'],
   'Orissa': ['Balangir', 'Baleswar', 'Bargarh', 'Bolangir', 'Cuttack', 'Dhenkanal', 'Ganjam', 'Kalahandi', 'Keonjhar', 'Koraput', 'Mayurbhanja', 'Puri', 'Sambalpur', 'Sundargarh'],
-  'Assam': ['Cachar', 'Darrang', 'Dibrugarh', 'Goalpara', 'Kamrup', 'Nagaon', ' Sibsagar', 'Sonitpur'],
+  'Assam': ['Cachar', 'Darrang', 'Dibrugarh', 'Goalpara', 'Kamrup', 'Nagaon', 'Sibsagar', 'Sonitpur'],
   'Chhattisgarh': ['Bilaspur', 'Bastar', 'Durg', 'Dhamtari', 'Kabirdham', 'Kanker', 'Korba', 'Mahasamund', 'Raigarh', 'Raipur', 'Rajnandgaon', 'Surguja'],
-  'Jharkhand': ['Bokaro', 'Dhanbad', 'Giridih', 'Hazaribagh', 'Ranchi', 'Santal Paragana / Dumka'],
+  'Jharkhand': ['Bokaro', 'Dhanbad', 'Giridih', 'Hazaribagh', 'Ranchi', 'Santhal Paragana / Dumka'],
   'Uttarakhand': ['Almora', 'Chamoli', 'Dehradun', 'Garhwal', 'Nainital', 'Pithorgarh', 'Tehri Garhwal', 'Uttar Kashi'],
   'Himachal Pradesh': ['Bilaspur', 'Chamba', 'Hamirpur', 'Kangra', 'Kinnaur', 'Kullu', 'Mandi', 'Shimla', 'Solan']
 };
